6ade5248fcb61db1f9d9f57dbc434a7643a9d8cc
anderson-fruhauf <andersonfruhauf14@gmail.com>
2019-08-29 22:21:40 -0300

    checkbox

--- src/aula2_23_08_checkbox/CheckBox.java
+++ src/aula2_23_08_checkbox/CheckBox.java
@@ -2,6 +2,8 @@ package aula2_23_08_checkbox;
 
 import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 
 import javax.swing.JCheckBox;
 import javax.swing.JFrame;
@@ -10,16 +12,48 @@ import javax.swing.JTextField;
 /**
  * CheckBox
  */
-public class CheckBox extends JFrame{
+public class CheckBox extends JFrame {
     private JTextField textField;
-    private JCheckBox jCheckBox;
-    private JCheckBox italixJCheckBox;
+    private JCheckBox boldjCheckBox;
+    private JCheckBox italicJCheckBox;
 
-    public CheckBox(){
+    public CheckBox() {
         super("check box teste");
         setLayout(new FlowLayout());
-        textField = new JTextField("mude o tamanho da fonte para 20");
+        textField = new JTextField("veja a fonte mudar", 20);
         textField.setFont(new Font("serif", Font.PLAIN, 14));
-                
+        add(textField);
+
+        boldjCheckBox = new JCheckBox("Bold");
+        italicJCheckBox = new JCheckBox("Italic");
+
+        add(boldjCheckBox);
+        add(italicJCheckBox);
+
+        CheckBoxHandler handler = new CheckBoxHandler();
+        boldjCheckBox.addItemListener(handler);
+        italicJCheckBox.addItemListener(handler);
+
+    }
+
+    private class CheckBoxHandler implements ItemListener {
+
+        @Override
+        public void itemStateChanged(ItemEvent e) {
+            Font fonte = null;
+
+            if (boldjCheckBox.isSelected() && italicJCheckBox.isSelected()) {
+                fonte = new Font("Serif", Font.BOLD + Font.ITALIC, 14);
+            } else if (boldjCheckBox.isSelected()) {
+                fonte = new Font("Serif", Font.BOLD, 14);
+            } else if (italicJCheckBox.isSelected()) {
+                fonte = new Font("Serif", Font.ITALIC, 14);
+            } else {
+                fonte = new Font("Serif", Font.PLAIN, 14);
+            }
+
+            textField.setFont(fonte);
+        }
+
     }
 }
--- src/aula2_23_08_checkbox/CheckBoxTest.java
+++ src/aula2_23_08_checkbox/CheckBoxTest.java
@@ -1,11 +1,18 @@
 package aula2_23_08_checkbox;
 
+import javax.swing.JFrame;
+
 /**
  * CheckBoxTest
  */
+
+@SuppressWarnings("serial")
 public class CheckBoxTest {
 
     public static void main(String[] args) {
-        
+        CheckBox checkBox = new CheckBox();
+        checkBox.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        checkBox.setSize(300, 100);
+        checkBox.setVisible(true);
     }
 }
